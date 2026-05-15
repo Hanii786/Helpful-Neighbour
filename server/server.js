02f3c5a35baec41d708a4b4e57a4fd49d6cdb392
Abdul Hanan <hananarain3199@gmail.com>
2026-05-15 19:36:39 +0500

    add vercel config for backend deployment

--- server/server.js
+++ server/server.js
@@ -23,3 +23,5 @@ app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
